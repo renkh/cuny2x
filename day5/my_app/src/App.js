a0059983eb662963e3b440719f7437d8d54ffae2
renkh <renavoo@gmail.com>
2018-06-06 11:13:28 -0400

    added delete functionality

--- day5/my_app/src/App.js
+++ day5/my_app/src/App.js
@@ -10,6 +10,15 @@ class App extends Component {
     }
   }
 
+  getIndex(value, arr) {
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i] === value) {
+        return i;
+      }
+    }
+    return
+  }
+
   handleChange(event) {
     const name = event.target.value;
     this.setState ({
@@ -31,25 +40,40 @@ class App extends Component {
     console.log("this new list of names: ", this.state.listOfNames)
   }
 
+  handleDelete(i, event){
+    console.log("this is the name in the handleDelete: ", i );
+    let aListOfNames = this.state.listOfNames;
+    var index = i;
+    let updatedListOfNames = aListOfNames.filter((_, i) => i !== index);
+    console.log("this is the updatedListOfNames in the handleDelete: ", updatedListOfNames );
+    this.setState({
+      listOfNames: updatedListOfNames
+    })
+    let text = this.refs.text;
+    text.value = "";
+    console.log("this new list of names in handleDelete: ", this.state.listOfNames)
+  }
+
   render() {
     const listOfNames = this.state.listOfNames;
     const name = listOfNames.map((name, i) => (<li key={i}>{name}</li>));
-
     return (
       <div className="App">
         <p className="App-intro">
-          Welcome to my React app!
+          React Simple To Do List
         </p>
         <div>
           <form onSubmit={this.handleSubmit.bind(this)}>
             <label>
-              Name:
+              To-Do:
               <input onChange={this.handleChange.bind(this)} type="text" name="name" ref="text"/>
             </label>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Add" />
           </form>
           <ul>
-            { (name) ? name: null }
+            {listOfNames.map((name, i) => (
+              <li key={i}>{name}<button onClick={this.handleDelete.bind(this, i)}>Delete</button></li>
+            ))}
           </ul>
         </div>
       </div>

74d49bb56958938c07cafdaab1eb896dc6b918c0
renkh <renavoo@gmail.com>
2018-06-13 10:50:41 -0400

    initial commit

---
+++ day12/weatherapp/server.js
@@ -0,0 +1,21 @@
+const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  // res.render will render our view, then send the equivalent HTML to the client.
+  res.render('index');
+})
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
+})
+
+app.listen(3000, function () {
+  console.log('App listening on http://localhost:3000')
+})

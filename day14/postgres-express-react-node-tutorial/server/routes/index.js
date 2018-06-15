a67ac7d82553e5409bf063864bd8ae59243a0c4c
renkh <renavoo@gmail.com>
2018-06-15 12:01:21 -0400

    created todo items

--- day14/postgres-express-react-node-tutorial/server/routes/index.js
+++ day14/postgres-express-react-node-tutorial/server/routes/index.js
@@ -7,4 +7,5 @@ module.exports = (app) => {
 
   app.post('/api/todos', todosController.create);
   app.get('/api/todos', todosController.list);
+  app.post('/api/todos/:todoId/items', todoItemsController.create);
 };
